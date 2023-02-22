6c766ad88b012b1a58edd63173198f699b1c9a7f
SergiRubio156 <srubiomor@gmail.com>
2023-02-22 17:16:03 +0100

    Script Respawn

---
+++ Assets/MINIJUEGO/Script/respawn.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    public List<GameObject> Objects = new List<GameObject>();
+
+
+    public float speed = 8f;
+    private float positionX;
+    private float timeRemaining = 0f;
+    public float time = 5f;
+
+    bool direction = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction)
+        {
+            positionX += speed * Time.deltaTime;
+
+            this.transform.position = new Vector2(positionX, 0);
+        }
+        else if(!direction)
+        {
+            positionX -= speed * Time.deltaTime;
+
+            this.transform.position = new Vector2(positionX, 0);
+        }
+        if (timeRemaining > 0)
+            timeRemaining -= Time.deltaTime * 2;
+        else
+        {
+            timeRemaining = Random.Range(2, 6); 
+            respawnObject();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hola");
+        direction =! direction;
+    }
+
+
+    public void respawnObject()
+    {
+        int number = Random.Range(1, 4);
+        Instantiate(Objects[number],this.transform.position,Quaternion.identity);
+    }
+}
