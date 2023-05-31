7d91be94fa9dae9e6302cf15ee64521a5eb595d3
SergiRubio156 <srubiomor@gmail.com>
2023-05-31 19:33:20 +0200

    31/5

--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -71,7 +71,7 @@ public class GameManager : MonoBehaviour
 
     void Awake()
     {
-        state = GameState.Playing;
+        state = GameState.Menu;
 
         DontDestroyOnLoad(this.gameObject);
 
