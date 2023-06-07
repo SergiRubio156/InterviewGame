a6f80c69f00a803a870eb64a0e04cd02c7f0e64a
Anna Cortés <120062389+anna3412@users.noreply.github.com>
2023-06-08 01:11:56 +0200

    cositas

--- Assets/Project/Scripts/2nd minijuego/RandomImageGenerator.cs
+++ Assets/Project/Scripts/2nd minijuego/RandomImageGenerator.cs
@@ -9,19 +9,18 @@ public class RandomImageGenerator : MonoBehaviour
     public Texture2D[] pinturas; // Array que contiene las pinturas que quieres mostrar
     public RawImage pinturaImage; // Referencia al componente RawImage para mostrar las pinturas
     public Texture2D[] cpu; // Array que contiene las imágenes de CPU que quieres mostrar
-    public RawImage cpuImage; // Referencia al componente RawImage para mostrar las imágenes de CPU
     public TextMeshProUGUI countText; // Referencia al componente de TextMeshProUGUI para el contador
     public Button fetButton; // Referencia al botón
     private string randomNumTextTag = "text"; // Tag del objeto de texto para el número aleatorio
 
     private int count;
+    private int previousCPUIndex = -1; // Índice de la imagen de CPU anterior
 
     private void Start()
     {
         fetButton.onClick.AddListener(OnClickFetButton);
         GenerateRandomImage();
         GenerateRandomPintura();
-        GenerateRandomCPU();
         count = 0;
     }
 
@@ -60,8 +59,31 @@ public class RandomImageGenerator : MonoBehaviour
 
     public void GenerateRandomCPU()
     {
+        GameObject[] textObjects = GameObject.FindGameObjectsWithTag(randomNumTextTag);
+
+        foreach (GameObject textObject in textObjects)
+        {
+            RawImage cpuRawImage = textObject.GetComponent<RawImage>();
+
+            if (cpuRawImage != null)
+            {
+                int randomIndex = GetUniqueRandomCPUIndex();
+                cpuRawImage.texture = cpu[randomIndex];
+            }
+        }
+    }
+
+    private int GetUniqueRandomCPUIndex()
+    {
         int randomIndex = Random.Range(0, cpu.Length);
-        cpuImage.texture = cpu[randomIndex];
+
+        while (randomIndex == previousCPUIndex)
+        {
+            randomIndex = Random.Range(0, cpu.Length);
+        }
+
+        previousCPUIndex = randomIndex;
+        return randomIndex;
     }
 
     public void IncrementCount()
@@ -76,7 +98,7 @@ public class RandomImageGenerator : MonoBehaviour
 
         foreach (TextMeshProUGUI text in randomNumTexts)
         {
-            if (text.tag == randomNumTextTag)
+            if (text.CompareTag(randomNumTextTag))
             {
                 float randomNumber = Random.Range(1f, 5f);
                 randomNumber = Mathf.Round(randomNumber * 2f) / 2f; // Redondea al número más cercano con incremento de 0.5 o 1
